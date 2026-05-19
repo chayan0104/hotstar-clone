7b82dd693439dc574c6a1e51ed838f8a543ef0da
chayan0104 <chayansamanta8@email.com>
2026-05-19 23:17:52 +0530

    chnages

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -3,17 +3,16 @@ import tmdbAxiosInstance from '../tmdbAxiosInstance'
 import './Row.css'
 function Row({title,fetchUrl}) {
 const [allMovies,setAllMovies]=useState([])
-console.log(fetchUrl);
 const base_url="https://image.tmdb.org/t/p/original/"
 
-const fetchData=async()=>{
-   const {data}= await tmdbAxiosInstance.get(fetchUrl)
-   setAllMovies(data.results)
-}
-console.log(allMovies);
 useEffect(()=>{
+    const fetchData=async()=>{
+       const {data}= await tmdbAxiosInstance.get(fetchUrl)
+       setAllMovies(data.results || [])
+    }
+
     fetchData()
-},[])
+},[fetchUrl])
 
   return (
     <div className='row'>
@@ -23,10 +22,7 @@ useEffect(()=>{
             {
 
                 allMovies.map((item,index)=>(
-                    <>
-                    
-                    
-                    <div className='ba'>
+                    <div className='ba' key={item.id ?? `${title}-${index}`}>
                        <div className='iim'> <img className='movie' src={`${base_url}/${item.poster_path}`} alt="" />
                         
                             <div className='back'>
@@ -40,19 +36,18 @@ useEffect(()=>{
                                         </div>
                                         <h2>{item.original_title}</h2>
                                         <div style={{display:"flex"}}>
-                                            <h3>{item.release_date.slice(0,4)}</h3>
+                                            <h3>{item.release_date?.slice(0,4) || 'N/A'}</h3>
                                             <h3> &nbsp;.&nbsp; </h3>
                                             <h3>Rating: {item.vote_average}</h3>
                                         </div>
                                         <p>
-                                            {item.overview.slice(0,80)}
+                                            {item.overview?.slice(0,80)}
                                         </p>
                                     </div>
             
                                 </div>
                                 </div>
                         </div>
-                    </>
                 ))
             }
            
